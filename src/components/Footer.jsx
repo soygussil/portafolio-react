43ebb267c1ace0675e81164441cae161d9da7ab1
soygussil <gussilv1995@gmail.com>
2022-06-05 14:38:25 -0500

    Terminé otros detalles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-[#050c16]'>
-        <footer className='text-center text-white py-5'>
+        <footer className='text-center text-white py-5 text-sm md:text-xl'>
             <p>Copyright &copy; 2022 todos los derechos reservados Gus-Dev</p>
         </footer>
     </div>

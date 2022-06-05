a40f6bec0898a27a40dc9f68d3b394013c90a5bb
soygussil <gussilv1995@gmail.com>
2022-06-05 17:04:37 -0500

    detalles footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='bg-[#050c16]'>
         <footer className='text-center text-white py-5 text-sm md:text-xl'>
-            <p>Copyright &copy; 2022 todos los derechos reservados
+            <p>Copyright &copy; 2022
             <a href="mailto: gussilv1995@gmail.com"> Gus-Dev</a> 
             </p>
             

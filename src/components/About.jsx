aa17698a66e27d1e4366c5e1a94f6c9e6bdbf94b
soygussil <gussilv1995@gmail.com>
2022-06-05 16:37:38 -0500

    repare titulo about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,8 @@ const About = () => {
         <div className='flex flex-col justify-center items-center w-full h-full'>
             <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
                 <div className='pb-8 pl-4'>
-                    <p className='text-4xl font-bold inline border-b-4 border-[#FF6B6B]'>Acerca de mí</p>
+                    <p className='text-2xl md:text-4xl  font-bold inline text-gray-300 border-b-4 border-[#FF6B6B]'>Acerca de mí</p>
+                    
                 </div>
         <div>
             
